1f727d308af9e44c468fa3cf3d30d9842548923e
Christel MVELE <mvelengoupechristel81@gmail.com>
2024-07-23 22:27:44 +0200

    api "crud"

--- src/main/java/christel/mvele/nutrisportBackend/controller/UserController.java
+++ src/main/java/christel/mvele/nutrisportBackend/controller/UserController.java
@@ -23,10 +23,42 @@ public class UserController {
         return ResponseEntity.ok(users);
     }
 
+    /**
+     * Updates an existing user with the specified ID.
+     *
+     * @param id the unique identifier of the user to be updated
+     * @param updatedUser the updated details of the user
+     * @return a ResponseEntity containing the updated user in the body and a status code of 200 (OK) upon successful update
+     */
     @PutMapping("/{id}")
     public ResponseEntity<Utilisateur> updateUser(@PathVariable Long id, @RequestBody Utilisateur updatedUser) {
         Utilisateur user = apiService.updateUser(id, updatedUser);
         return ResponseEntity.ok(user);
+    }
+
+    /**
+     * Deletes a user with the specified ID.
+     *
+     * @param id the unique identifier of the user to be deleted
+     * @return a ResponseEntity with status code 204 (No Content) upon successful deletion
+     * @throws IllegalArgumentException if the provided ID is null or negative
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Integer id) {
+        apiService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    /**
+     * Adds a new user.
+     *
+     * @param newUser the details of the new user to be added
+     * @return a ResponseEntity containing the added user in the body and a status code of 201 (Created) upon successful addition
+     */
+    @PostMapping("/add")
+    public ResponseEntity<Utilisateur> addUser(@RequestBody Utilisateur newUser) {
+        Utilisateur user = apiService.addUser(newUser);
+        return ResponseEntity.status(201).body(user);
     }
 
 }
--- src/main/java/christel/mvele/nutrisportBackend/service/ApiService.java
+++ src/main/java/christel/mvele/nutrisportBackend/service/ApiService.java
@@ -2,10 +2,14 @@ package christel.mvele.nutrisportBackend.service;
 
 import christel.mvele.nutrisportBackend.model.Utilisateur;
 import christel.mvele.nutrisportBackend.repository.RoleRepository;
+import christel.mvele.nutrisportBackend.repository.TokenRepository;
 import christel.mvele.nutrisportBackend.repository.UtilisateurRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -15,8 +19,16 @@ import java.util.stream.Collectors;
 public class ApiService {
 
     private final UtilisateurRepository utilisateurRepository;
+    private final TokenRepository tokenRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
-
+    /**
+     * Retrieves all users from the database and returns a list of user objects.
+     * The returned list contains user objects with only the necessary attributes for display purposes.
+     *
+     * @return a list of user objects with only the necessary attributes for display purposes
+     */
     public List<Utilisateur> getAllUsers() {
         List<Utilisateur> users = (List<Utilisateur>) utilisateurRepository.findAll();
         return users.stream()
@@ -32,6 +44,14 @@ public class ApiService {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Updates an existing user in the database with the provided updated user details.
+     *
+     * @param id the unique identifier of the user to be updated
+     * @param updatedUser the updated user details containing the new values for the user's name, email, and other attributes
+     * @return the updated user object after saving it to the database
+     * @throws RuntimeException if the user with the given id is not found in the database
+     */
     public Utilisateur updateUser(Long id, Utilisateur updatedUser) {
         Optional<Utilisateur> existingUserOptional = utilisateurRepository.findById(updatedUser.getId());
         if (existingUserOptional.isPresent()) {
@@ -43,6 +63,52 @@ public class ApiService {
         } else {
             throw new RuntimeException("User not found with id " + id);
         }
+    }
+
+    /**
+     Deletes a user from the database by their id.
+     @param id the unique identifier of the user to be deleted
+     @throws RuntimeException if the user with the given id is not found in the database
+     */
+    @Transactional
+    public void deleteUser(Integer id) {
+        Optional<Utilisateur> existingUserOptional = utilisateurRepository.findById(id);
+        if (existingUserOptional.isPresent()) {
+            tokenRepository.deleteByUtilisateurId(id);
+            utilisateurRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("User not found with id " + id);
+        }
+    }
+
+    /**
+     * Adds a new user to the database.
+     *
+     * @param newUser the new user to be added
+     * @return the added user object
+     * @throws RuntimeException if a user with the same email already exists
+     */
+    public Utilisateur addUser(Utilisateur newUser) {
+        Optional<Utilisateur> existingUserOptional = utilisateurRepository.findByMail(newUser.getMail());
+        var userRole = roleRepository.findByName("USER")
+                .orElseThrow(() -> new IllegalStateException("Role User not initialized"));
+        if (existingUserOptional.isPresent()) {
+            throw new RuntimeException("User with email " + newUser.getMail() + " already exists");
+        }else {
+            newUser = Utilisateur.builder()
+                    .nom(newUser.getNom())
+                    .prenom(newUser.getPrenom())
+                    .mdp(passwordEncoder.encode(newUser.getMdp()))
+                    .mail(newUser.getMail())
+                    .photo(newUser.getPhoto())
+                    .accountLocked(false)
+                    .enabled(true)
+                    .roles(List.of(userRole))
+                    .createdDate(LocalDate.now())
+                    .build();
+            return utilisateurRepository.save(newUser);
+        }
+        //return utilisateurRepository.save(newUser);
     }
 
 }
